481fe44209056f052f4d6e9d8cb55974fb5a00ba
mathieu.valette1 <mathieu.valette1@gmail.com>
2022-02-14 12:12:26 +0100

    ajout des c/c

--- capitalix/src/App.js
+++ capitalix/src/App.js
@@ -1,27 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
-
-const [services, setServices] = useState(new Services(""))
-const [world, setWorld] = useState(new World())
+import {Services} from "./Services.ts";
+import {useEffect, useState} from "react";
+import {World} from "./world.ts";
 
 
 function App() {
+
+  const [services, setServices] = useState(new Services(""))
+  const [world, setWorld] = useState(new World())
+
+  useEffect(() => {
+
+    let services = new Services("")
+    setServices(services)
+    services.getWorld().then(response => {
+          setWorld(response.data)
+        }
+    )
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div> logo monde</div>
+        <div> argent</div>
+        <div> multiplicateur</div>
+        <div> ID du joueur</div>
+      </div>
+      <div className="main">
+        <div> liste des boutons de menu</div>
+        <div className="product">
+          <div> premier produit</div>
+          <div> second produit</div>
+          <div> troisième produit</div>
+          <div> quatrième produit</div>
+          <div> cinquième produit</div>
+          <div> sixième produit</div>
+        </div>
+      </div>
     </div>
   );
 }
