7e43393faf904a6da55be855dd58573dcff6ae5f
DenoelaGuennoc <dguennoc@gmail.com>
2022-02-22 09:54:27 +0100

    Mise en place

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Services } from './service';
+import { World } from './world';
+//import React, { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [services, setServices] = useState(new Services(""))
+    const [world, setWorld] = useState(new World())
+    const username = ""
+    useEffect(() => {
+
+        let services = new Services(username)
+        setServices(services)
+        services.getWorld().then(response => {
+        setWorld(response.data)
+        }
+        )
+       
+    }, [])
+    return (
+        <div className="App">
+            <div className="header">
+                <div> logo monde <img src={services.server + world.logo}/></div>
+                <div> argent </div>
+                <div> multiplicateur </div>
+                <div> ID du joueur </div>
+            </div>
+            <div className="main">
+                <div> liste des boutons de menu </div>
+            <div className="product">
+                <div> premier produit </div>
+                <div> second produit </div>
+                <div> troisième produit </div>
+                <div> quatrième produit </div>
+                <div> cinquième produit </div>
+                <div> sixième produit </div>
+            </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
