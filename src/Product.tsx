ddd18cfa20e88369f1626a6720072e3d37a1f0a5
mathieu.valette1 <mathieu.valette1@gmail.com>
2022-03-08 15:43:42 +0100

    progress bar

--- src/Product.tsx
+++ src/Product.tsx
@@ -1,18 +1,30 @@
+import { useState } from "react"
 import { Services } from "./service"
 import { Product, World } from "./world"
+import ProgressBar from "./ProgressBar"
 
 type ProductProps = {
     prod: Product
     services: Services
 }
 export default function ProductComponent({ prod, services }: ProductProps) {
+    const [progress, setProgress] = useState(0)
+
     return (
         <div>
-            <img className="productLogo" src={services.server + prod.logo} alt={prod.logo}/><span> {prod.name} </span>
+            <img onClick={startFabrication} className="productLogo" src={services.server + prod.logo} alt={prod.logo}/><span> {prod.name} </span>
             <div><span>Quantité: {prod.quantite} </span>
                 <span>Temps: {prod.timeleft} </span>
                 <span>Revenu: {prod.revenu} </span></div>
             <div><span>Prix: {prod.cout} </span><span>Vitesse: {prod.vitesse}</span></div>
+            <div className="progressBar">
+                <ProgressBar transitionDuration={"0.1s"} customLabel={" "}
+                             completed={progress}/>
+            </div>
         </div>
     )
+}
+
+function startFabrication(){
+    console.log("Icone cliquée")
 }
