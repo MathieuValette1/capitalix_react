dd2eccfa2da8a601e2f9ffc2bba403bbbbf5b2fe
mathieu.valette1 <mathieu.valette1@gmail.com>
2022-03-08 09:57:25 +0100

    affichage product

--- src/Product.tsx
+++ src/Product.tsx
@@ -8,7 +8,11 @@ type ProductProps = {
 export default function ProductComponent({ prod, services }: ProductProps) {
     return (
         <div>
-            ...
+            <img className="productLogo" src={services.server + prod.logo} alt={prod.logo}/><span> {prod.name} </span>
+            <div><span>Quantité: {prod.quantite} </span>
+                <span>Temps: {prod.timeleft} </span>
+                <span>Revenu: {prod.revenu} </span></div>
+            <div><span>Prix: {prod.cout} </span><span>Vitesse: {prod.vitesse}</span></div>
         </div>
     )
 }
